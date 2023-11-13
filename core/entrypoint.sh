c8c6e050d168de17003632517afa4f15a3525b65
Abhisht <abhishtchouhan@gmail.com>
2023-11-14 00:25:42 +0530

    Updated DockerFile

--- core/entrypoint.sh
+++ core/entrypoint.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
 export STEAMPIPE_DATABASE_PASSWORD=admin
-# export STEAMPIPE_INTROSPECTION=info
-# steampipe service start --show-password
-# steampipe service stop
 steampipe service start --show-password
-python3 app.py
-# python3 /app/app.py
-# steampipe service start --foreground --show-password
-
-# while true; do
-#     sleep 10
-# done
+# python3 app.py
+python3 /app/app.py
